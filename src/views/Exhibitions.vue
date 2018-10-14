18dfddcf01d012b67fedc13fb942c8e219fe5934
Patsakorn Towatrakool <tusaveeiei@gmail.com>
2018-10-15 01:07:24 +0700

    showed exhibition list

--- src/views/Exhibitions.vue
+++ src/views/Exhibitions.vue
@@ -1,12 +1,34 @@
 <template>
   <div>
-    Exhibition  
+    <div>
+      Exhibition  
+    </div>
+    <ol>
+      <li v-for="(exhibition, index) in exhibitions" :key="index">
+        {{exhibition['name']}}  
+        {{exhibition['start-date']}}
+        {{exhibition['end-date']}}
+        <ul>
+          <li v-for="(artwork, index) in exhibition['display']" :key="index">
+            {{artwork}}
+          </li>
+        </ul>
+        {{exhibition['number-supported-users']}}
+      </li>
+    </ol>
   </div>
 </template>
 
 <script>
+import { exhibitions } from '../assets/database/exhibitions.json';
+
 export default {
-  name: 'Exhibitions'
+  name: 'Exhibitions',
+  data() {
+    return {
+      exhibitions
+    };
+  }
 };
 </script>
 

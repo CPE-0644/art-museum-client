e97e3e95b40a4eec71c944b7fdb7b597856d103d
Patsakorn Towatrakool <tusaveeiei@gmail.com>
2018-10-14 17:25:56 +0700

    created header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
-  <Home />
+  <div id="app">
+    <Header />
+    <Home />
+    <Footer />
+  </div>
 </template>
 
 <script>
 import Home from './views/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default {
   name: 'app',
   components: {
+    Header,
+    Footer,
     Home
   }
 };

f1f34672210bf8ba1fa776f0d69c810337293cd1
Patsakorn Towatrakool <tusaveeiei@gmail.com>
2018-10-15 17:58:31 +0700

    show artists

--- src/views/Artists.vue
+++ src/views/Artists.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-    Artists  
+    <h1>Artist</h1>
+    <ul>
+      <li v-for="(artist, index) in artists" :key="index">{{artist}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import { artists } from '../assets/database/artists.json';
+
 export default {
-  name: 'Artists'
+  name: 'Artists',
+  data() {
+    return {
+      artists: artists.slice(0, 10)
+    };
+  }
 };
 </script>
 

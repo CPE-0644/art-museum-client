f63da284a7f4fe0afa8ae67a1bc4f8a2062c0145
Patsakorn Towatrakool <tusaveeiei@gmail.com>
2018-10-18 16:43:05 +0700

    used element-ui

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+    <el-menu-item index="1">Processing Center</el-menu-item>
+    <el-submenu index="2">
+      <template slot="title">Workspace</template>
+      <el-menu-item index="2-1">item one</el-menu-item>
+      <el-menu-item index="2-2">item two</el-menu-item>
+      <el-menu-item index="2-3">item three</el-menu-item>
+      <el-submenu index="2-4">
+        <template slot="title">item four</template>
+        <el-menu-item index="2-4-1">item one</el-menu-item>
+        <el-menu-item index="2-4-2">item two</el-menu-item>
+        <el-menu-item index="2-4-3">item three</el-menu-item>
+      </el-submenu>
+    </el-submenu>
+    <el-menu-item index="3" disabled>Info</el-menu-item>
+    <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item>
+  </el-menu>
     <h1>
       Museum
     </h1>
@@ -15,7 +31,7 @@
     </div>
     <div>
       <router-link to="/artists">Artist</router-link>
-</div>
+    </div>
   </div>
 </template>
 
